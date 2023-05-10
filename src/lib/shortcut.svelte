4cfac54c2db523bd9d6839a01fe7ef41ea14ec34
Eric Brown <eric.brown8787@gmail.com>
2023-05-10 17:23:47 -0400

    Set up Svelte project

---
+++ src/lib/shortcut.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let url = 'https://www.google.com/';
+	export let linkLabel = 'Google';
+	import img from './img/alien-icon.svg';
+	export let alt = 'An icon';
+</script>
+
+<a href={url}>
+	<img src={img} {alt} />
+	<p>{linkLabel}</p>
+</a>
+
+<style>
+	img {
+		max-width: 50px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Shortcut from '../lib/shortcut.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<section class="shortcuts">
+	<Shortcut url="https://mail.google.com/" linkLabel="Gmail" alt="Gmail Icon" />
+	<Shortcut url="https://www.github.com/" linkLabel="Github" alt="Github Icon" />
+</section>
+
+<style>
+	.links {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+</style>
